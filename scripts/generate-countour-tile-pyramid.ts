ea60d710750c389ed05c0443a0873b18b1373faa
acalcutt <acalcutt@techidiots.net>
2024-12-20 13:24:26 -0500

    attempt to get local pmtiles working

---
+++ scripts/generate-countour-tile-pyramid.ts
@@ -0,0 +1,233 @@
+import { writeFileSync, mkdir } from "fs";
+import type { DemTile, Encoding } from "../dist/types";
+import exported from "../dist/index";
+import { extractZXYFromUrlTrim , GetImageData, getPMtilesTile, openPMtiles} from "./node-pmtiles-adapter";	
+import { getChildren } from "@mapbox/tilebelt";
+import path from "path";
+
+type Tile = [number, number, number];
+
+
+// Define an interface for parsed arguments
+interface ParsedArgs {
+  x: number;
+  y: number;
+  z: number;
+  sFile: string;
+  sEncoding: string;
+  sMaxZoom: number;
+  increment: number;
+  oMaxZoom: number;
+  oDir: string;
+}
+
+// Get command-line arguments
+const args: string[] = process.argv.slice(2);
+
+// Helper function to parse command-line arguments
+function parseArgs(): ParsedArgs {
+  let x: number | undefined;
+  let y: number | undefined;
+  let z: number | undefined;
+  let sFile: string | undefined;
+  let sEncoding: string | undefined;
+  let sMaxZoom: number | undefined;
+  let increment: number | undefined;
+  let oMaxZoom: number | undefined;
+  let oDir: string | undefined;
+
+  for (let i = 0; i < args.length; i += 2) {
+    const argName = args[i];
+    const argValue = args[i + 1];
+    if (argName === "--x") {
+      x = parseInt(argValue);
+    } else if (argName === "--y") {
+      y = parseInt(argValue);
+    } else if (argName === "--z") {
+      z = parseInt(argValue);
+    } else if (argName === "--sFile") {
+      sFile = argValue;
+    } else if (argName === "--sEncoding") {
+      sEncoding = argValue;
+    } else if (argName === "--sMaxZoom") {
+      sMaxZoom = parseInt(argValue);
+    } else if (argName === "--increment") {
+      increment = parseInt(argValue);
+    } else if (argName === "--oMaxZoom") {
+      oMaxZoom = parseInt(argValue);
+    } else if (argName === "--oDir") {
+      oDir = argValue;
+    }
+  }
+
+  // Error handling, default values
+  if (isNaN(x as number)) {
+    throw new Error("Invalid --x argument. Must be a number.");
+  }
+  if (isNaN(y as number)) {
+    throw new Error("Invalid --y argument. Must be a number.");
+  }
+  if (isNaN(z as number)) {
+    throw new Error("Invalid --z argument. Must be a number.");
+  }
+  if (!sFile) {
+    throw new Error("Invalid --sFile argument. Must be a string.");
+  }
+  if (!sEncoding) {
+    throw new Error("Invalid --sEncoding argument. Must be a string.");
+  }
+  if (isNaN(sMaxZoom as number)) {
+    throw new Error("Invalid --sMaxZoom argument. Must be a number.");
+  }
+  if (isNaN(increment as number)) {
+    throw new Error("Invalid --increment argument. Must be a number.");
+  }
+  if (isNaN(oMaxZoom as number)) {
+    throw new Error("Invalid --oMaxZoom argument. Must be a number.");
+  }
+  if (!oDir) {
+    throw new Error("Invalid --oDir argument. Must be a string.");
+  }
+
+  return {
+    x: x as number,
+    y: y as number,
+    z: z as number,
+    sFile: sFile as string,
+    sEncoding: sEncoding as string,
+    sMaxZoom: sMaxZoom as number,
+    increment: increment as number,
+    oMaxZoom: oMaxZoom as number,
+    oDir: oDir as string,
+  };
+}
+
+// Parse command line args and set defaults
+let x: number,
+  y: number,
+  z: number,
+  sFile: string,
+  sEncoding: string,
+  sMaxZoom: number,
+  increment: number,
+  oMaxZoom: number, // Renamed maxZoom to oMaxZoom
+  oDir: string;
+try {
+  ({ x, y, z, sFile, sEncoding, sMaxZoom, increment, oMaxZoom, oDir } =
+    parseArgs());
+} catch (e: any) {
+  console.error(e);
+  console.error(
+    "Usage: npx tsx ./src/generate-countour-tile-pyramid.ts --x <x> --y <y> --z <z> --sFile <sFile> --sEncoding <sEncoding> --sMaxZoom <sMaxZoom> --increment <increment> --oMaxZoom <oMaxZoom> --oDir <oDir>",
+  );
+  process.exit(1);
+}
+
+function getAllTiles(tile: Tile, maxZoom: number): Tile[] {
+  let allTiles: Tile[] = [tile]; // Initialize with the input tile
+
+  function getTileList(tile: Tile) {
+    const children: Tile[] = getChildren(tile);
+    allTiles = allTiles.concat(children);
+    if (children.length === 0 || children[0][2] >= maxZoom) return;
+    children.forEach(getTileList);
+  }
+
+  getTileList(tile);
+
+  return allTiles;
+}
+
+async function processTile(v: Tile): Promise<void> {
+  const z: number = v[2];
+  const x: number = v[0];
+  const y: number = v[1];
+  const dirPath: string = path.join(oDir, `${z}`, `${x}`);
+  const filePath: string = path.join(dirPath, `${y}.pbf`);
+
+  return manager
+    .fetchContourTile(z, x, y, { levels: [increment] }, new AbortController())
+    .then((tile) => {
+      return new Promise<void>((resolve, reject) => {
+        mkdir(dirPath, { recursive: true }, (err) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          writeFileSync(filePath, Buffer.from(tile.arrayBuffer));
+          resolve();
+        });
+      });
+    });
+}
+
+async function processQueue(
+  queue: Tile[],
+  batchSize: number = 25,
+): Promise<void> {
+  for (let i = 0; i < queue.length; i += batchSize) {
+    const batch = queue.slice(i, i + batchSize);
+    console.log(
+      `Processing batch ${i / batchSize + 1} of ${Math.ceil(queue.length / batchSize)} of tile ${z}/${x}/${y}`,
+    );
+    await Promise.all(batch.map(processTile));
+    console.log(
+      `Processed batch ${i / batchSize + 1} of ${Math.ceil(queue.length / batchSize)} of tile ${z}/${x}/${y}`,
+    );
+  }
+}
+
+async function GetTileFunction(
+  url: string,
+  abortController: AbortController,
+) {
+  const options: RequestInit = {
+    signal: abortController.signal,
+  };
+ 
+  const $zxy = extractZXYFromUrlTrim(url)
+  if (!$zxy) {
+    throw new Error(`Could not extract zxy from ${url}`);
+  }
+
+  const zxyTile = await getPMtilesTile(pmtiles, $zxy.z, $zxy.x, $zxy.y)
+  if (!zxyTile || !zxyTile.data) {
+    throw new Error(`No tile returned for ${url}`);
+  }
+
+  const blob = new Blob([zxyTile.data]);
+  return {
+    data: blob,
+    expires: undefined,
+    cacheControl: undefined,
+  };
+};
+
+
+const pmtiles = openPMtiles(sFile);
+
+const manager = new exported.LocalDemManager({
+    demUrlPattern: "https://{z}/{x}/{y}.png", //Does not need to be real, since we are replacing fetch.
+    cacheSize: 100,
+    encoding: sEncoding as Encoding,
+    maxzoom: sMaxZoom,
+    timeoutMs: 10000,
+    decodeImage: GetImageData,
+	  getTile: GetTileFunction,
+});
+
+// Use parsed command line args
+const children: Tile[] = getAllTiles([x, y, z], oMaxZoom);
+
+children.sort((a, b) => {
+  //Sort by Z first
+  if (a[2] !== b[2]) return a[2] - b[2];
+  //If Z is equal, sort by X
+  if (a[0] !== b[0]) return a[0] - b[0];
+  //If Z and X are equal, sort by Y
+  return a[1] - b[1];
+});
+
+processQueue(children).then(() => {
+  console.log(`All files for tile ${z}/${x}/${y} have been written!`);
+});
---
+++ scripts/node-pmtiles-adapter.ts
@@ -0,0 +1,136 @@
+import fs from "node:fs";
+import { PMTiles, FetchSource, type Source } from "pmtiles";
+import { PNG } from "pngjs";
+import exported from "../dist/index";
+import type { DemTile, Encoding } from "../dist/types";
+
+const httpTester = /^https?:\/\//i;
+
+export class PMTilesFileSource implements Source {
+  private fd: number;
+
+  constructor(fd: number) {
+    this.fd = fd;
+  }
+
+  getKey(): string {
+    return String(this.fd); // Convert the fd to a string
+  }
+
+  async getBytes(
+    offset: number,
+    length: number,
+  ): Promise<{ data: ArrayBuffer }> {
+    const buffer = Buffer.alloc(length);
+    await readFileBytes(this.fd, buffer, offset);
+    const ab = buffer.buffer.slice(
+      buffer.byteOffset,
+      buffer.byteOffset + buffer.byteLength,
+    );
+    return { data: ab };
+  }
+}
+
+async function readFileBytes(
+  fd: number,
+  buffer: Buffer,
+  offset: number,
+): Promise<void> {
+  return new Promise((resolve, reject) => {
+    fs.read(fd, buffer, 0, buffer.length, offset, (err, bytesRead, buff) => {
+      if (err) {
+        return reject(err);
+      }
+      if (bytesRead !== buffer.length) {
+        return reject(
+          new Error(
+            `Failed to read the requested amount of bytes, got ${bytesRead} expected ${buffer.length}`,
+          ),
+        );
+      }
+      resolve();
+    });
+  });
+}
+
+export function openPMtiles(FilePath: string): PMTiles {
+  let pmtiles: PMTiles;
+  let fd: number | undefined;
+
+  try {
+    if (httpTester.test(FilePath)) {
+      const source = new FetchSource(FilePath);
+      pmtiles = new PMTiles(source);
+    } else {
+      fd = fs.openSync(FilePath, "r");
+      const source = new PMTilesFileSource(fd);
+      pmtiles = new PMTiles(source);
+    }
+    return pmtiles;
+  } finally {
+  }
+}
+
+export async function getPMtilesTile(
+  pmtiles: PMTiles,
+  z: number,
+  x: number,
+  y: number,
+): Promise<{ data: ArrayBuffer | undefined }> {
+  try {
+    const zxyTile = await pmtiles.getZxy(z, x, y);
+
+    if (zxyTile && zxyTile.data) {
+      return { data: zxyTile.data };
+    } else {
+      return { data: undefined };
+    }
+  } catch (error) {
+    console.error("Error fetching tile:", error);
+    return { data: undefined };
+  }
+}
+
+export async function GetImageData(
+  blob: Blob,
+  encoding: Encoding,
+  abortController: AbortController,
+): Promise<DemTile> {
+  const buffer = await blob.arrayBuffer();
+  const png = PNG.sync.read(Buffer.from(buffer));
+  const parsed = exported.decodeParsedImage(png.width, png.height, encoding, png.data as any as Uint8ClampedArray);
+  if (Boolean(abortController?.signal?.aborted)) return null as any as DemTile;
+  return parsed;
+}
+
+export function extractZXYFromUrlTrim(url: string): { z: number; x: number; y: number } | null {
+  // 1. Find the index of the last `/`
+  const lastSlashIndex = url.lastIndexOf("/");
+  if (lastSlashIndex === -1) {
+    return null; // URL does not have any slashes
+  }
+
+    const segments = url.split("/");
+    if (segments.length <= 3){
+        return null;
+    }
+
+    const ySegment = segments[segments.length-1];
+    const xSegment = segments[segments.length-2];
+    const zSegment = segments[segments.length-3];
+
+    const lastDotIndex = ySegment.lastIndexOf(".");
+    const cleanedYSegment = lastDotIndex === -1 ? ySegment : ySegment.substring(0, lastDotIndex);
+
+  // 3. Attempt to parse segments as numbers
+  const z = parseInt(zSegment, 10);
+  const x = parseInt(xSegment, 10);
+  const y = parseInt(cleanedYSegment, 10);
+
+
+  if (isNaN(z) || isNaN(x) || isNaN(y)) {
+    return null; // Conversion failed, invalid URL format
+  }
+
+  return { z, x, y };
+}
